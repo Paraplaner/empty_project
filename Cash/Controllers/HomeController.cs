f647dbdeca7b81d5176c828f1389fb78e6a97004
Baranov <baraserg@gmail.com>
2017-09-15 17:50:57 +0300

    Добавлен минимальный asp.net mvc проект в качестве шаблона для начала разработки.

---
+++ Cash/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Cash.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return Content("paraplaner cash webapp");
+        }
+    }
+}
